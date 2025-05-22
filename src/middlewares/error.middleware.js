d731ef942a92d41a0713df67255b81bdd209a0ed
AyushAJS <jangidayush89@gmail.com>
2025-05-22 16:26:31 +0530

    resolve the error

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,32 +1,13 @@
-
-const errorMiddleware = (err, req, res, next)=>{
+const errorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
-
     err.msg = err.msg || "Something went wrong";
-
-    return res.status(err.status).json({
-        success: false,
-        message: err.msg,
-        track: err.track,
-    })
-}
-
-export default errorMiddleware;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.msg,
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
+    });
+  };
+  
+  export default errorMiddleware;
+  
